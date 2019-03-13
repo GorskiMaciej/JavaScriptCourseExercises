03b9add57a110e17413744af89f66f845fb9ba4a
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-03-13 21:28:00 +0100

    script after refactor

--- Sec05_ex02/main.js
+++ Sec05_ex02/main.js
@@ -15,24 +15,12 @@ const init = () => {
 
 const createLiElements = () => {
     const ul = document.querySelector('ul');
-    const li1 = document.createElement('li');
-    const li2 = document.createElement('li');
-    const li3 = document.createElement('li');
-    const li4 = document.createElement('li');
-    const li5 = document.createElement('li');
-    const li6 = document.createElement('li');
-    const li7 = document.createElement('li');
-    const li8 = document.createElement('li');
-    const li9 = document.createElement('li');
-    const li10 = document.createElement('li');
-    const liArray = [li1, li2, li3, li4, li5, li6, li7, li8, li9, li10];
 
-    for (let i = 0; i < liArray.length; i++) {
-        ul.appendChild(liArray[i]);
-        liArray[i].textContent = `Element nr ${orderElement}`;
-        liArray[i].style.fontSize = `${size}px`;
-        size++;
-        orderElement++;
+    for (let i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.textContent = `Element nr ${orderElement++}`;
+        li.style.fontSize = `${size++}px`;
     }
 
 }
