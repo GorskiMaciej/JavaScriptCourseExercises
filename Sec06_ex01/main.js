29948357f49fda3e10ccc8e538f55290b1738088
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-03-14 19:13:12 +0100

    Finished exercise

--- Sec06_ex01/main.js
+++ Sec06_ex01/main.js
@@ -1,5 +1,29 @@
-// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
+let text = document.createElement('p');
+document.body.appendChild(text);
+let positionX = null;
+let positionY = null;
+text.textContent = `${positionX},${positionY}`;
+text.style.position = "fixed";
+text.style.top = "50%";
+text.style.left = "50%";
+text.style.transform = "translate(-50%,-50%)";
+text.style.fontSize = "50px";
 
-// jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
-// jesli wartość dla x i y jest nieparzysta to kolor niebieski
-// jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
+document.body.addEventListener('click', (evnt) => {
+    positionX = evnt.clientX;
+    console.log(positionX);
+    positionY = evnt.clientY;
+    console.log(positionY);
+
+    let evenX = !(positionX % 2);
+    let evenY = !(positionY % 2);
+
+    if (evenX && evenY) {
+        document.body.style.backgroundColor = "red";
+    } else if (!evenX && !evenY) {
+        document.body.style.backgroundColor = "blue";
+    } else {
+        document.body.style.backgroundColor = "green";
+    };
+    text.textContent = `${positionX},${positionY}`;
+});
