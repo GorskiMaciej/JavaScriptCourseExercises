77ed276294cedfd2b4b90bbd69ac2f798324289a
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-04-25 22:03:04 +0200

    finished exercise

--- Sec09_ex01/game.js
+++ Sec09_ex01/game.js
@@ -17,7 +17,6 @@ const gameSummary = {
 
 function handSelection() {
     actualGame.playerSelection = this.dataset.option;
-    ++gameSummary.numberOfGames;
     images.forEach(image => image.style.boxShadow = "none")
     this.style.boxShadow = "0 0 0 3px #f00";
 }
@@ -36,13 +35,41 @@ function checkResult(player, computer) {
     }
 }
 
+function publishResult(player, computer, result) {
+    let winner = document.querySelector('[data-summary="who-win"]');
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = computer;
+    document.querySelector('.numbers span').textContent = gameSummary.numberOfGames++;
+
+    if (result === "draw") {
+        document.querySelector('.draws span').textContent = ++gameSummary.draws;
+        winner.textContent = "Remis :/";
+        winner.style.color = "grey";
+    } else if (result === "win") {
+        document.querySelector('.wins span').textContent = ++gameSummary.wins;
+        winner.textContent = "Gracz! :)";
+        winner.style.color = "green";
+    } else {
+        document.querySelector('.losses span').textContent = ++gameSummary.losses;
+        winner.textContent = "Komputer :(";
+        winner.style.color = "red";
+    }
+
+}
+
+function resetActualGame() {
+    document.querySelector(`[data-option ="${actualGame.playerSelection}"]`).style.boxShadow = "none";
+    actualGame.playerSelection = "";
+};
+
 function start() {
     if (!actualGame.playerSelection) {
         return alert("proszę wybrać symbol");
     }
     actualGame.computerSelection = randomComputerChoise();
     const gameResult = checkResult(actualGame.playerSelection, actualGame.computerSelection)
-    console.log(gameResult);
+    publishResult(actualGame.playerSelection, actualGame.computerSelection, gameResult);
+    resetActualGame();
 }
 
 images.forEach(image => image.addEventListener('click', handSelection));
