af92715cf171cae19bef6e76d94e0f7ef4059162
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-08-03 21:59:53 +0200

    Sec06ex04 - shorter solution

--- Sec06ex04/main.js
+++ Sec06ex04/main.js
@@ -4,64 +4,37 @@ let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
-const changeColor = (e) => {
-    if (e.keyCode === 38) {
-        if (red < 255) {
-            red += 3;
-            green += 3;
-            blue += 3;
-        } else {
-            red = 255;
-            green = 255;
-            blue = 255;
-        }
-        console.log(red)
-        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    } else if (e.keyCode === 40) {
-        if (red > 0) {
-            red -= 3;
-            green -= 3;
-            blue -= 3;
-        } else {
-            red = 0;
-            green = 0;
-            blue = 0;
-        }
-        console.log(red)
-        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    }
-};
+// const changeColor = (e) => {
+//     if (e.keyCode === 38 && red < 255) {
+
+//         red += 3;
+//         green += 3;
+//         blue += 3;
+
+//         console.log(red)
+//         document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+//     } else if (e.keyCode === 40 && red > 0) {
+//         red -= 3;
+//         green -= 3;
+//         blue -= 3;
+
+//         console.log(red)
+//         document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+//     }
+// };
 
 const changeColor = (e) => {
     switch (e.keyCode) {
-        case 38: {
-            if (red < 255) {
-                red += 3;
-                green += 3;
-                blue += 3;
-            } else {
-                red = 255;
-                green = 255;
-                blue = 255;
-            }
-            console.log(red)
-            document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        case 38:
+            document.body.style.backgroundColor = `rgb(${red<255?red+=3:red},${green<255?green+=3:green},${blue<255?blue+=3:blue})`;
+            console.log(red);
             break;
-        }
-        case 40: {
-            if (red > 0) {
-                red -= 3;
-                green -= 3;
-                blue -= 3;
-            } else {
-                red = 0;
-                green = 0;
-                blue = 0;
-            }
-            console.log(red)
-            document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
+        case 40:
+            document.body.style.backgroundColor = `rgb(${red>0?red-=3:red},${green>0?green-=3:green},${blue>0?blue-=3:blue})`;
+            console.log(red);
             break;
-        }
+
     }
 };
 
