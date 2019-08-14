6addb861366acd27b32a3901d504ba33450dade0
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-08-14 23:25:46 +0200

    code refactor with tutor

--- Sec13ex03-stoper/main.js
+++ Sec13ex03-stoper/main.js
@@ -2,8 +2,6 @@ const starBtn = document.querySelector('.main');
 const resetBtn = document.querySelector('.reset');
 const timeDisplay = document.querySelector('.time div');
 let time = 0;
-let seconds = 0;
-let hundreths = 0;
 let indexOfInterval = 0;
 let startFlag = false;
 
@@ -11,12 +9,8 @@ const start = () => {
     starBtn.textContent = "Pause"
     if (!startFlag) {
         indexOfInterval = setInterval(() => {
-            time += 1;
-            hundreths = time % 100;
-            hundreths = hundreths < 10 ? `0${hundreths}` : `${hundreths}`;
-            if (hundreths == 0) hundreths = "00";
-            seconds = Math.floor(time / 100);
-            timeDisplay.textContent = `${seconds}.${hundreths}s`;
+            time++;
+            timeDisplay.textContent = `${(time/100).toFixed(2)}s`;
         }, 10)
         startFlag = !startFlag;
     } else {
@@ -29,8 +23,6 @@ const start = () => {
 const reset = () => {
     clearInterval(indexOfInterval);
     time = 0;
-    seconds = 0;
-    hundreths = 0;
     timeDisplay.textContent = "---";
     starBtn.textContent = "Start"
     startFlag = false;
